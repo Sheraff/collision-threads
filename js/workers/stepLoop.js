33386509002fb66877019b7e5eedc6182e1583dc
Flo <florian.pellet@matera.eu>
2022-06-05 20:59:33 +0200

    remove setAsap

--- js/workers/stepLoop.js
+++ js/workers/stepLoop.js
@@ -37,21 +37,6 @@ export function loop() {
 	frame()
 }
 
-const asapChannel = new MessageChannel()
-function setAsap(fn, delay) {
-	const time = performance.now()
-	asapChannel.port1.onmessage = () => {
-		const now = performance.now()
-		const delta = now - time
-		if (delta < delay) {
-			setAsap(fn, delay - delta)
-		} else {
-			fn()
-		}
-	}
-	asapChannel.port2.postMessage(null)
-}
-
 export function getUps() {
 	const ups = upsArray.length / upsArray.reduce((a, b) => a + b)
 	return Math.round(ups)
